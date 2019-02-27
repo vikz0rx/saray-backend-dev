f7a14410c0a61cc0f9346a1479c13144b9b8e232
Viktor Zagoruyko <vik.zgrk@gmail.com>
2019-02-27 21:09:24 +0500

    Setting up permissions and stuff in Admin Panel

--- SarayBackend/main/admin.py
+++ SarayBackend/main/admin.py
@@ -1,14 +1,61 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from mediumeditor.admin import MediumEditorAdmin
 from .models import *
 
 @admin.register(SarayUser)
 class SarayUserAdmin(admin.ModelAdmin):
-    list_display = ['firstname', 'lastname', 'email', 'birthdate']
+    def phone_refactored(self, obj):
+        if obj.phone:
+            return '+' + obj.phone[0] + '(' + obj.phone[1:4] + ')' + obj.phone[4:7] + '-' + obj.phone[7:]
+        return '-'
+
+    phone_refactored.short_description = 'Номер телефона'
+
+    def name_refactored(self, obj):
+        if obj.firstname and obj.lastname and obj.fathersname:
+            return obj.lastname + ' ' + obj.firstname + ' ' + obj.fathersname
+        elif obj.firstname and obj.lastname:
+            return obj.firstname + ' ' + obj.lastname
+        else:
+            return obj.username
+
+    name_refactored.short_description = 'ФИО'
+
+    list_display = ['name_refactored', 'phone_refactored', 'email', 'birthdate']
+    search_fields = ('lastname', )
+
+    def get_form(self, request, obj=None, **kwargs):
+        if not request.user.is_superuser:
+            self.exclude = ('username', 'is_superuser', 'user_permissions', 'groups', 'password', 'is_active', 'is_staff', 'last_login')
+        else:
+            self.exclude = ('password', 'user_permissions', 'last_login')
+
+        if obj.is_staff:
+            self.exclude += ('sms_notification', 'mail_notification', 'allow_to_use_photos')
+
+        return super(SarayUserAdmin, self).get_form(request, obj, **kwargs)
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            if request.user != obj:
+                if obj.is_staff:
+                    return ('last_login', 'username', 'email', 'phone', 'image', 'firstname', 'lastname', 'fathersname', 'birthdate', 'passport_series', 'passport_number', 'insurance', 'is_superuser', 'is_staff', 'groups', )
+                else:
+                    return ('last_login', 'username', 'email', 'phone', 'image', 'firstname', 'lastname', 'fathersname', 'birthdate', 'passport_series', 'passport_number', 'insurance', 'sms_notification', 'mail_notification', 'allow_to_use_photos', 'is_superuser', 'is_staff', 'groups', )
+            else:
+                return ('last_login', 'is_superuser', 'is_staff', 'groups', )
+        else:
+            return []
 
 @admin.register(Locations)
 class LocationsAdmin(MediumEditorAdmin, admin.ModelAdmin):
-    list_display = ['title']
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+
+    image_tag.short_description = ''
+
+    list_display = ['title', 'image_tag']
 
     mediumeditor_fields = ('text', )
 
@@ -18,13 +65,20 @@ class PhotographsAdmin(admin.ModelAdmin):
 
 @admin.register(News)
 class NewsAdmin(MediumEditorAdmin, admin.ModelAdmin):
-    list_display = ['author', 'title', 'created_at', 'approved']
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+    
+    image_tag.short_description = 'Обложка'
+
+    list_display = ['author', 'title', 'created_at', 'image_tag', 'approved']
+    readonly_fields = ('author', )
+    search_fields = ('title', )
 
     mediumeditor_fields = ('text', )
 
     def get_form(self, request, obj=None, **kwargs):
         if not request.user.is_superuser:
-            self.exclude = ('author', 'approved')
+            self.exclude = ('approved', )
         return super(NewsAdmin, self).get_form(request, obj, **kwargs)
 
     def save_model(self, request, obj, form, change):
@@ -32,11 +86,10 @@ class NewsAdmin(MediumEditorAdmin, admin.ModelAdmin):
             obj.author = request.user
         obj.save()
 
-    def queryset(self, request):
+    def get_queryset(self, request):
         if request.user.is_superuser:
-            return Entry.objects.all()
-        print(request.user)
-        return Entry.objects.filter(author=request.user)
+            return News.objects.all()
+        return News.objects.filter(author=request.user)
 
     def has_change_permission(self, request, obj=None):
         has_class_permission = super(NewsAdmin, self).has_change_permission(request, obj)
@@ -64,4 +117,32 @@ class BookingOptionsAdmin(admin.ModelAdmin):
 
 @admin.register(Bookings)
 class BookingsAdmin(admin.ModelAdmin):
-    list_display = ['date', 'location', 'time_start', 'time_end']
+    def send_notification(self, request, queryset):
+        print(request)
+        print(queryset)
+    
+    send_notification.short_description = "Отправить дополнительное уведомление"
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.location.image.url))
+
+    image_tag.short_description = ''
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            # SEND SMS / MAIL NOTIFICATION FUNCTION
+            obj.payment_notification = True
+
+            # CALCULATE SUM FUNCTION
+            sum = 0
+            sum += obj.location.cost 
+            sum += obj.types.cost 
+            # SUM SELECTED OPTIONS
+            # print(obj.options.all())
+            obj.cost = sum
+
+        obj.save()
+
+    actions = ['send_notification']
+    list_display = ['date', 'location', 'image_tag', 'time_start', 'time_end', 'payment_notification', 'reminder_notification', 'cost', 'status']
+    readonly_fields = ('payment_notification', 'reminder_notification', 'cost', )
--- SarayBackend/main/models.py
+++ SarayBackend/main/models.py
@@ -34,6 +34,8 @@ class SarayUserManager(BaseUserManager):
 class SarayUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(_('Имя пользователя'), db_index=True, max_length=255, unique=True)
     email = models.EmailField(_('Электропочта'), db_index=True, unique=True)
+    phone = models.CharField(_('Номер телефона'), max_length=11, null=True, blank=True)
+    image = models.FileField(_('Изображение'), upload_to='headshots', null=True, blank=True)
     
     firstname = models.CharField(_('Имя'), max_length=32, null=True, blank=True)
     lastname = models.CharField(_('Фамилия'), max_length=32, null=True, blank=True)
@@ -41,8 +43,13 @@ class SarayUser(AbstractBaseUser, PermissionsMixin):
 
     birthdate = models.DateField(_('Дата рождения'), null=True, blank=True)
     
-    passport_series = models.CharField(_('Серия паспорта'), max_length=32, null=True, blank=True)
-    passport_number = models.CharField(_('Номер паспорта'), max_length=32, null=True, blank=True)
+    passport_series = models.CharField(_('Серия паспорта'), max_length=4, null=True, blank=True)
+    passport_number = models.CharField(_('Номер паспорта'), max_length=6, null=True, blank=True)
+    insurance = models.CharField(_('СНИЛС'), max_length=11, null=True, blank=True)
+
+    sms_notification = models.BooleanField(_('Оповещение по SMS'), default=False)
+    mail_notification = models.BooleanField(_('Оповещение по электропочте'), default=True)
+    allow_to_use_photos = models.BooleanField(_('Разрешение на использование фотографий'), default=True)
 
     is_active = models.BooleanField(_('Активный аккаунт'), default=True)
     is_staff = models.BooleanField(_('Административный аккаунт'), default=False)
@@ -56,7 +63,9 @@ class SarayUser(AbstractBaseUser, PermissionsMixin):
     objects = SarayUserManager()
 
     def __str__(self):
-        if self.firstname and self.lastname:
+        if self.firstname and self.lastname and self.fathersname:
+            return self.firstname + ' ' + self.lastname + ' ' + self.fathersname
+        elif self.firstname and self.lastname:
             return self.firstname + ' ' + self.lastname
         else:
             return self.username
@@ -82,9 +91,9 @@ class SarayUser(AbstractBaseUser, PermissionsMixin):
         return token.decode('utf-8')
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['is_superuser', 'is_staff', '-created_at']
         verbose_name = _('пользователя фотостудии')
-        verbose_name_plural = _('Пользователя фотостудии')
+        verbose_name_plural = _('Пользователи фотостудии')
 
 class Locations(models.Model):
     title = models.CharField(_('Название локации'), max_length=32)
@@ -104,7 +113,7 @@ class Photographs(models.Model):
     last_name = models.CharField(_('Фамилия'), max_length=32)
     desc = models.CharField(_('Описание'), max_length=64)
     link = models.CharField(_('Instagram'), max_length=64)
-    image = models.FileField(_('Фотография'), upload_to='photographs')
+    image = models.FileField(_('Фотография'), upload_to='headshots')
 
     def __str__(self):
         return self.first_name + ' ' + self.last_name
@@ -125,7 +134,7 @@ class News(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['-approved', '-created_at']
         verbose_name = _('новость')
         verbose_name_plural = _('Новости')
 
@@ -154,17 +163,46 @@ class BookingOptions(models.Model):
         verbose_name_plural = _('Бронирования / Дополнительные услуги')
 
 class Bookings(models.Model):
+    IS_CREATED = "IN_PROGRESS"
+    IS_PAYED = "IS_PAYED"
+
+    STATUS_CHOICES = (
+        (IS_CREATED, "Не оплачен"),
+        (IS_PAYED, "Оплачен"),
+    )
+
+
     date = models.DateField()
     time_start = models.TimeField(_('Начало'), blank=True, null=True)
     time_end = models.TimeField(_('Конец'), blank=True, null=True)
+    status = models.CharField(_('Статус'), max_length=16, choices=STATUS_CHOICES, default=IS_CREATED)
+    
     location = models.ForeignKey(Locations, on_delete=models.CASCADE, related_name='location')
     photograph = models.ForeignKey(Photographs, on_delete=models.CASCADE, blank=True, related_name='photograph')
     types = models.ForeignKey(BookingTypes, on_delete=models.CASCADE, blank=True, related_name='type')
-    options = models.ManyToManyField(BookingOptions, blank=True)
+    options = models.ManyToManyField(BookingOptions, blank=True, related_name='options_choice')
+
+    payment_notification = models.BooleanField(_('Оповещение I'), default=False)
+    reminder_notification = models.BooleanField(_('Оповещение II'), default=False)
+
+    cost = models.SmallIntegerField(_('Стоимость'), blank=True, null=True)
 
     def __str__(self):
         return str(self.date)
 
     class Meta:
+        ordering = ['-status', '-date']
         verbose_name = _('бронирование')
         verbose_name_plural = _('Бронирования')
+
+# IS_PAYED = "IS_PAYED"
+# IN_PROGRESS = "IN_PROGRESS"
+# IS_DONE = "IS_DONE"
+
+# STATUS_CHOICES = (
+#     (IS_PAYED, "Оплачен"),
+#     (IN_PROGRESS, "Выполняется"),
+#     (IS_DONE, "Завершен"),
+# )
+
+# status = models.CharField(_('Статус'), max_length=16, choices=STATUS_CHOICES, default=IS_PAYED)
